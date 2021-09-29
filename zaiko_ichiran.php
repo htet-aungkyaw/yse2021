024bb45a31b96b32ae2775ac102a21833aadb875
htet-aungkyaw <200138nr@yse-c.net>
2021-09-29 11:06:52 +0900

    login

--- zaiko_ichiran.php
+++ zaiko_ichiran.php
@@ -1,108 +1,85 @@
 <?php
 /* 
 【機能】
-書籍テーブルより書籍情報を取得し、画面に表示する。
-商品をチェックし、ボタンを押すことで入荷、出荷が行える。
-ログアウトボタン押下時に、セッション情報を削除しログイン画面に遷移する。
+	　ユーザ名とパスワードを元に認証を行う。認証についてはソースコードに
+	直接記述されているユーザ名とパスワードが一致しているかを確認する。
+	一致していた場合はログインして書籍一覧を表示し、ログインできない
+	場合はエラーとする。
 
 【エラー一覧（エラー表示：発生条件）】
-入荷する商品が選択されていません：商品が一つも選択されていない状態で入荷ボタンを押す
-出荷する商品が選択されていません：商品が一つも選択されていない状態で出荷ボタンを押す
+	名前かパスワードが未入力です：IDまたはパスワードが未入力
+	ユーザー名かパスワードが間違っています：①IDが間違っている　②IDが正しいがパスワードが異なる
+	ログインしてください：ログインしていない状態で他のページに遷移した場合(ログイン画面に遷移し上記を表示)
 */
+//⑥セッションを開始する
 
-//①セッションを開始する
-
-//②SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-if (/* ②の処理を書く */){
-	//③SESSIONの「error2」に「ログインしてください」と設定する。
-	//④ログイン画面へ遷移する。
+//①名前とパスワードを入れる変数を初期化する
+$name = "";
+$password = "";
+/*
+ * ②ログインボタンが押されたかを判定する。
+ * 押されていた場合はif文の中の処理を行う
+ */
+if (isset($_POST["decission"]) && $_POST["decision"] == 1) {
+	/*
+	 * ③名前とパスワードが両方とも入力されているかを判定する。
+	 * 入力されていた場合はif文の中の処理を行う。
+	 */
+	// if (/* ③の処理を書く */) {
+	// 	//④名前とパスワードにPOSTで送られてきた名前とパスワードを設定する
+	// } else {
+		//⑤名前かパスワードが入力されていない場合は、「名前かパスワードが未入力です」という文言をメッセージを入れる変数に設定する
+	// }
 }
 
-//⑤データベースへ接続し、接続情報を変数に保存する
+//⑦名前が入力されているか判定する。入力されていた場合はif文の中に入る
+// if (/* ⑦の処理を書く */) {
+	//⑧名前に「yse」、パスワードに「2021」と設定されているか確認する。設定されていた場合はif文の中に入る
+// 	if (/* ⑧の処理を書く */){
+ 		//⑨SESSIONに名前を設定し、SESSIONの「login」フラグをtrueにする
+	//⑩在庫一覧画面へ遷移する
+// 		header(/* ⑩の遷移先を書く */);
+// 	}else{
+	//⑪名前もしくはパスワードが間違っていた場合は、「ユーザー名かパスワードが間違っています」という文言をメッセージを入れる変数に設定する
+// 	}
+// }
 
-//⑥データベースで使用する文字コードを「UTF8」にする
-
-//⑦書籍テーブルから書籍情報を取得するSQLを実行する。また実行結果を変数に保存する
+ //⑫SESSIONの「error2」に値が入っているか判定する。入っていた場合はif文の中に入る
+// if (/* ⑫の処理を書く */) {
+	//⑬SESSIONの「error2」の値をエラーメッセージを入れる変数に設定する。
+	//⑭SESSIONの「error2」にnullを入れる。
+// }
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-	<meta charset="UTF-8">
-	<title>書籍一覧</title>
-	<link rel="stylesheet" href="css/ichiran.css" type="text/css" />
+<meta charset="UTF-8">
+<title>ログイン</title>
+<link rel="stylesheet" href="css/login.css" type="text/css" />
 </head>
-<body>
-	<div id="header">
-		<h1>書籍一覧</h1>
-	</div>
-	<form action="zaiko_ichiran.php" method="post" id="myform" name="myform">
-		<div id="pagebody">
-			<!-- エラーメッセージ表示 -->
-			<div id="error">
-				<?php
-				/*
-				 * ⑧SESSIONの「success」にメッセージが設定されているかを判定する。
-				 * 設定されていた場合はif文の中に入る。
-				 */ 
-				if(/* ⑧の処理を書く */){
-					//⑨SESSIONの「success」の中身を表示する。
-				}
-				?>
-			</div>
-			
-			<!-- 左メニュー -->
-			<div id="left">
-				<p id="ninsyou_ippan">
-					<?php
-						echo @$_SESSION["account_name"];
-					?><br>
-					<button type="button" id="logout" onclick="location.href='logout.php'">ログアウト</button>
-				</p>
-				<button type="submit" id="btn1" formmethod="POST" name="decision" value="3" formaction="nyuka.php">入荷</button>
-
-				<button type="submit" id="btn1" formmethod="POST" name="decision" value="4" formaction="syukka.php">出荷</button>
-			</div>
-			<!-- 中央表示 -->
-			<div id="center">
-
-				<!-- 書籍一覧の表示 -->
-				<table>
-					<thead>
-						<tr>
-							<th id="check"></th>
-							<th id="id">ID</th>
-							<th id="book_name">書籍名</th>
-							<th id="author">著者名</th>
-							<th id="salesDate">発売日</th>
-							<th id="itemPrice">金額</th>
-							<th id="stock">在庫数</th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php
-						//⑩SQLの実行結果の変数から1レコードのデータを取り出す。レコードがない場合はループを終了する。
-						while(/* ⑩の処理を書く */){
-							//⑪extract変数を使用し、1レコードのデータを渡す。
-
-							echo "<tr id='book'>";
-							echo "<td id='check'><input type='checkbox' name='books[]'value="./* ⑫IDを設定する */."></td>";
-							echo "<td id='id'>/* ⑬IDを表示する */</td>";
-							echo "<td id='title'>/* ⑭titleを表示する */</td>";
-							echo "<td id='author'>/* ⑮authorを表示する */</td>";
-							echo "<td id='date'>/* ⑯salesDateを表示する */</td>";
-							echo "<td id='price'>/* ⑰priceを表示する */</td>";
-							echo "<td id='stock'>/* ⑱stockを表示する */</td>";
-
-							echo "</tr>";
-						}
-						?>
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</form>
-	<div id="footer">
-		<footer>株式会社アクロイト</footer>
+<body id="login">
+	<div id="main">
+		<h1>ログイン</h1>
+		<?php 
+		//⑮エラーメッセージの変数に入っている値を表示する 
+		//echo "<div id='error'>", /* ⑮の変数を書く */, "</div>";
+		
+		//⑯メッセージの変数に入っている値を表示する 
+		//echo "<div id='msg'>", /* ⑯の変数を書く */, "</div>"; 
+		?>
+		<form action="login.php" method="post" id="log">
+			<p>
+				<input type='text' name="name" size='5' placeholder="Username">
+			</p>
+			<p>
+				<input type='password' name='pass' size='5' maxlength='20'
+					placeholder="Password">
+			</p>
+			<p>
+				<button type="submit" formmethod="POST" name="decision" value="1"
+					id="button">Login</button>
+			</p>
+		</form>
 	</div>
 </body>
 </html>
