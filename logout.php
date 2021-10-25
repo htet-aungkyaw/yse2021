027788db7b1ee0540016b347d736a5b9d68fc4cc
htet-aungkyaw <200138nr@yse-c.net>
2021-10-25 10:55:53 +0900

    lougout

--- logout.php
+++ logout.php
@@ -6,19 +6,10 @@
 //①セッションを開始する。
 session_start();
 //②セッションを削除する。
-if(isset($_SESSION["user"])) {
-unset($_SESSION["id"]);
-unset($_SESSION["name"]);
+if(isset($_SESSION["login"])) {
+unset($_SESSION["user"]);
+unset($_SESSION["login"]);
+}
 //③ログイン画面へ遷移する。
 header("Location: login.php");
-session_start();
-
-$_SESSION = array();
-
-session_destroy();
-
-header("location: login.php");
-
 exit;
-}
-?>
